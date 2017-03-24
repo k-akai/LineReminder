73e55e6f2d6b8b30e712aeedca6bdc4e9994e8a4
akai <akai.kazuyuki@nttcom.co.jp>
2017-03-24 05:31:40 +0000

    1

--- routes/form1.js
+++ routes/form1.js
@@ -5,7 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('form1.html', {
 	title: '登録画面',
-	name1: 'たろう'})
+	id:'30',
+	user: 'かずゆき',
+	location:'本町',
+	school:'海神小学校',
+	child1name:'ゆきと',
+	child1birth:'2012/12/20',
+	child2name:'まどか',
+	child2birth:'2014/04/30'
+    })
 
 });
 
--- routes/form1reg.js
+++ routes/form1reg.js
@@ -1,9 +1,45 @@
+var share =require('../lib/share.js');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('form1reg.html', { title: '登録完了' })
+    if (!req.query.id) {
+	res.render('form1reg.html', { title: '登録ERROR' });
+	return;
+    }
+
+    var db=share.db;
+    var lineuser = db.collection("lineuser")
+    var cursor = lineuser.find();
+    cursor.toArray(function(err, docs){
+        // toArray用のコールバック関数
+        if(err){
+	    console.error('読み込みエラー');
+	    throw(err);
+	}
+	console.log(docs);
+    });
+
+    //すでにデータがあるかをチェックする
+    var jsonw={"id":req.query.id,"location":req.query.location}
+    console.log(req.query.id)
+    console.log(jsonw)
+    /*
+    lineuser.insert(
+	json,
+	// 書き込み処理後のコールバック関数（省略可）
+	function(err, data){
+	    if(err){
+		console.error('書き込み時にエラーが発生しました');
+	        throw(err);
+	    }
+	    console.log('データ書き込み完了:',data);
+	    res.render('form1reg.html', { title: '登録完了' });
+
+	});
+     */
+    res.render('form1reg.html', { title: '登録完了' });
 
 });
 
